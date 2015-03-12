71e5e87b0d3045307510f0d1f7af1e7a7cc7e156
Robin Kanters <robin@robinkanters.nl>
2015-03-12 19:59:58 +0100

    Made some basic model classes

---
+++ PostcodeApi/Model/BagInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+namespace PostcodeApi.Model
+{
+    [DataContract]
+    public class BagInfo
+    {
+        [DataMember(Name = "id")]
+        public string Id { get; private set; }
+
+        [DataMember(Name = "type")]
+        public string Type { get; private set; }
+
+        [DataMember(Name = "purpose")]
+        public string Purpose { get; private set; }
+
+        internal BagInfo()
+        {
+        }
+    }
+}
---
+++ PostcodeApi/Model/Postcode.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.Serialization;
+
+namespace PostcodeApi.Model
+{
+    [DataContract]
+    public class Postcode
+    {
+        [DataMember(Name = "street")]
+        public string Street { get; private set; }
+
+        [DataMember(Name = "town")]
+        public string Town { get; private set; }
+
+        [DataMember(Name = "municipality")]
+        public string Municipality { get; private set; }
+
+        [DataMember(Name = "postcode")]
+        public string PostcodeQuery { get; private set; }
+
+        [DataMember(Name = "longitude")]
+        public float Longitude { get; private set; }
+
+        [DataMember(Name = "latitude")]
+        public float Latitude { get; private set; }
+
+        [DataMember(Name = "bag")]
+        public BagInfo BagInfo { get; private set; }
+
+        /// <summary>
+        /// Restricting access to constructor
+        /// </summary>
+        internal Postcode()
+        {
+        }
+
+        internal Postcode(string postcode, string street, string town, string municipality, float longitude, float latitude)
+        {
+            Street = street;
+            Town = town;
+            Municipality = municipality;
+            PostcodeQuery = postcode;
+            Longitude = longitude;
+            Latitude = latitude;
+        }
+    }
+}
