fa740e50af7c3a5659423a31c578859908131ba0
Robin Kanters <robin@robinkanters.nl>
2015-03-14 13:21:10 +0100

    Basic API

---
+++ PostcodeApi/PostcodeApi.cs
@@ -0,0 +1,26 @@
+﻿using PostcodeApi.DAO;
+using PostcodeApi.Model;
+
+namespace PostcodeApi
+{
+    public class PostcodeApi
+    {
+        public string ApiKey { private get; set; }
+
+        public PostcodeApi(string apiKey)
+        {
+            ApiKey = apiKey;
+        }
+
+        /// <summary>
+        /// Find information about a postcode by supplying it as a string
+        /// </summary>
+        /// <param name="query">The postcode to query</param>
+        /// <returns></returns>
+        public Postcode GetPostcodeInformation(string query)
+        {
+            PostcodeDao dao = new PostcodeDao(ApiKey);
+            return dao.Query(query).Resource;
+        }
+    }
+}
